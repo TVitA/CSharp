2613e86f88912b1275e3f702c95328a998b6f1c7
Talataj Vitaliy <talatajvitaliyandreevich@gmail.com>
2021-05-05 11:14:25 +0300

    Start work with async programming...

---
+++ AsyncMethods_async_await/AsyncMethods_async_await/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AsyncMethods_async_await
+{
+    internal static class Program : Object
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine("Starting Main method...");
+
+            Console.WriteLine("Test async method...");
+
+            Console.WriteLine("Ending Main method...");
+        }
+
+        private static async void FactorialAsync()
+        {
+            Console.WriteLine("Starting FactorialAsync method...");
+
+
+
+            Console.WriteLine("Ending FactorialAsync method...");
+        }
+    }
+}
