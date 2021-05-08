2c4a560d775e404b87fcf3db3a56b3652e084886
Talataj Vitaliy <talatajvitaliyandreevich@gmail.com>
2021-05-08 16:07:25 +0300

    ~

--- AsyncProgramming/AsyncMethods_async_await/Program.cs
+++ AsyncProgramming/AsyncMethods_async_await/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncMethods_async_await
 {
@@ -10,16 +12,43 @@ namespace AsyncMethods_async_await
 
             Console.WriteLine("Test async method...");
 
+            Console.WriteLine("Call FactorialAsync...");
+
+            FactorialAsync();
+
+            Console.WriteLine("Input some string:");
+
+            String input = Console.ReadLine();
+
+            Console.WriteLine("You entered '{0}'", input);
+
             Console.WriteLine("Ending Main method...");
+
+            Console.ReadLine();
         }
 
         private static async void FactorialAsync()
         {
             Console.WriteLine("Starting FactorialAsync method...");
 
+            Thread.Sleep(2000);
 
+            Int32 factorial = await Task<Int32>.Run(Factorial);
+
+            Console.WriteLine("5! = {0}", factorial);
 
             Console.WriteLine("Ending FactorialAsync method...");
+        }
+
+        private static Int32 Factorial()
+        {
+            Console.WriteLine("Factorial start computing...");
+
+            Thread.Sleep(8000);
+
+            Console.WriteLine("Factorial end computing...");
+
+            return 120;
         }
     }
 }
