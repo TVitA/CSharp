33abaa01913c37dd6d33109df4eb35e790259de7
TVitA <toukakirishima20012019@gmail.com>
2021-05-16 23:24:15 +0300

    ~]

--- AsyncProgramming/AsyncMethods_async_await_3/Program.cs
+++ AsyncProgramming/AsyncMethods_async_await_3/Program.cs
@@ -7,7 +7,29 @@ namespace AsyncMethods_async_await_3
     {
         private static void Main(String[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (var i = 0; i < 10; ++i)
+            {
+                FactorialAsync(i);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static Int32 Factorial(Int32 n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return (n > 1) ? n * Factorial(n - 1) : 1;
+        }
+
+        private static async void FactorialAsync(Int32 n)
+        {
+            Int32 factorial = await Task<Int32>.Run(() => Factorial(n));
+
+            Console.WriteLine("Factorial {0} = {1}", n, factorial);
         }
     }
 }
